4739f7b9311fede2d5755779ea543c23fbec4135
Vraj Barot <vrajbarot@gmail.com>
2022-09-11 15:06:30 +0800

    Misc. changes

--- Feature Based/testing.py
+++ Feature Based/testing.py
@@ -24,24 +24,25 @@ from functions import (
 
 # OCT_File_List.reverse()
 
-img0_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\output.png"
-img1_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Sample Images\Image_Set_1\OCT Images\OCT_p5.png"
-img2_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Sample Images\Image_Set_1\OCT Images\OCT_p6.png"
+img0_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Feature Based\1.png"
+# img0_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Sample Images\Image_Set_1\OCT Images\OCT_p6.png"
+img1_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Sample Images\Image_Set_1\OCT Images\OCT_p4.png"
+# img2_path = r"C:\Users\Vraj\Documents\Thesis\Image-Algorithm\Sample Images\Image_Set_1\OCT Images\OCT_p6.png"
 
 img0 = cv.imread(img0_path)  # 6 prime
 img1 = cv.imread(img1_path)  # Image 5
-img2 = cv.imread(img2_path)  # Image 6
+# img2 = cv.imread(img2_path)  # Image 6
 
 
 img0_gray = cv.cvtColor(img0, cv.COLOR_RGB2GRAY)
 img1_gray = cv.cvtColor(img1, cv.COLOR_RGB2GRAY)
-img2_gray = cv.cvtColor(img2, cv.COLOR_RGB2GRAY)
+# img2_gray = cv.cvtColor(img2, cv.COLOR_RGB2GRAY)
 
 # Calculate Normalised Crosscorrelation of 2 images
-score = crossCorellation(img0, img2)
-print(score)
+# score = crossCorellation(img0, img2)
+# print(score)
 
-blended = overlayImages(img0, img2)
+# blended = overlayImages(img0, img2)
 
 # test = absDifference(img0, img1)
 # cv.imshow("test", test)
@@ -55,16 +56,19 @@ blended = overlayImages(img0, img2)
 
 # print(result)
 
-# features0 = featureExtraction(img0_gray)
-# features1 = featureExtraction(img1_gray)
+features0 = featureExtraction(img0_gray)
+features1 = featureExtraction(img1_gray)
 # features2 = featureExtraction(img2_gray)
 
 
-# matches = featureMatching(features0, features1)
-# matches2 = featureMatching(features2, features1)
-
+matches = featureMatching(features0, features1)
 
 # print(matches)
+
+print(features0.matched_pts)
+print(features1.matched_pts)
+
+
 # print(matches2)
 
 # homography, _ = cv.findHomography(
